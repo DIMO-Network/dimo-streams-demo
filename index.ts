8a60818b0a8df6e7d3df5db4efa0c3daed51cc12
Yevgeny Khessin <yev@dimo.zone>
2024-04-09 19:21:54 -0400

    Adding example of dev license subscribe

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 import StreamrClient from "@streamr/sdk"
-import crypto from "crypto";
-const streamId = "streams.dimo.eth/firehose/weather";
+import { StreamPartIDUtils } from '@streamr/protocol'
+import { ethers } from "ethers";
+const streamId = "streams.dimo.eth/vehicles/3";
 
 const main = async () => {
   // Create the client using the validated private key
   const client = new StreamrClient({
+    logLevel: 'debug',
     auth: {
-      privateKey: crypto.randomBytes(32).toString('hex')
+      //this is the signer private key the developer adds
+      privateKey: '00000'
     },
   });
-
+  console.log("Client public key is:", client.getAddress());  
   const stream = await client.getStream(streamId);
 
   const onMessage = (content) => {
@@ -18,8 +21,10 @@ const main = async () => {
   
   const subscriptions = await Promise.all(
     stream.getStreamParts().map(async (partition) => {
+      const [id, part] = StreamPartIDUtils.getStreamIDAndPartition(partition)
       await client.subscribe(
-        partition,
+        //the erc1271 contract in this api is the contract address of the Developer License ClientId
+        { id: id, partition: part, erc1271Contract: "0x401aEf4801eF3691447B80400560Ce750FF63FB0" },
         onMessage
       );
     })
